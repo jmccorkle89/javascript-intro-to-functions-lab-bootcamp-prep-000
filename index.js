aa0a6968ac44319adbd283fba4b37c78ede78ec5
jmccorkle89@yahoo.com <jmccorkle89@yahoo.com>
2018-04-24 03:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,3 @@ function sayHiToGrandma(string) {
   if (string === "hello")
   return `${"I can\'t hear you!"}`;
 }
-
-function sayHiToGrandma(string) {
-  if (string === "HELLO")
-  return `${"YES INDEED!"}`;
-}
