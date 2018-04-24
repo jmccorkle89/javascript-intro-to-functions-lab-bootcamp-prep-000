5137ebf6c8b09ae838e13ad46a64f1963d480f64
jmccorkle89@yahoo.com <jmccorkle89@yahoo.com>
2018-04-24 03:51:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "hello")
   return `${"I can\'t hear you!"}`;
-  else return `${"YES INDEED!"}`;
+  else 
+  return `${"YES INDEED!"}`;
 }
 
